703e14cc6b6d05e535f0861821701a346d02ad42
cowsoncows <coffedrop@live.com>
2023-06-28 22:35:03 -0400

    Add home link to navbar title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import TextLink from './TextLink.js';
 import Button from './Button.js';
 import { useState, useEffect } from 'react';
 import HamburgerMenu from './HamburgerMenu.js';
+import Link from 'next/link.js';
 
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
@@ -21,11 +22,12 @@ const Navbar = () => {
   return (
     <div className={`fixed w-full grid place-items-center z-50 bg-secondary ${open && 'shadow-xl'}`}>
       <nav className='grid grid-cols-2 max-w-screen-xl items-center w-full mx-auto p-4'>
-        <h1
+        <Link
+          href='/'
           className={`col-span-1 leading-none inline font-poppins text-white font-semibold text-lg md:text-2xl lg:text-3xl leading-none transition-all ${visible || open ? 'opacity-100' : 'opacity-0'}`}
         >
           Nicholas Brodbeck
-        </h1>
+        </Link>
 
         {/* Desktop View */}
         <div className='hidden flex-row justify-end items-center gap-6 md:flex'>
