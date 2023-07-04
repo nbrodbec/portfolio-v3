2a48cc8a89425f6170be300b7c594c17c56ef9bc
cowsoncows <coffedrop@live.com>
2023-07-03 21:20:43 -0400

    Remove unused dependencies

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,5 @@
 import '@/styles/globals.css'
 import Layout from '../components/Layout.js';
-import { Head } from 'next/document.js';
 
 export default function App({ Component, pageProps }) {
   return (
