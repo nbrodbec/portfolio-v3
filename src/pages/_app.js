9d16cd068a53c9bebfe446df356b28c8d04160f2
cowsoncows <coffedrop@live.com>
2023-07-03 21:17:38 -0400

    Remove title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,13 +4,8 @@ import { Head } from 'next/document.js';
 
 export default function App({ Component, pageProps }) {
   return (
-    <div>
-      <Head>
-        <title>Nicholas Brodbeck</title>
-      </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </div>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
