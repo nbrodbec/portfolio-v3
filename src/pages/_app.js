bfb2c7682127ae8cb5ad1ed65c0b16f1d559af47
cowsoncows <coffedrop@live.com>
2023-07-03 21:14:14 -0400

    Add title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,16 @@
 import '@/styles/globals.css'
 import Layout from '../components/Layout.js';
+import { Head } from 'next/document.js';
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <div>
+      <Head>
+        <title>Nicholas Brodbeck</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
   );
 }
