dc1d1a51f08f74a0f39af35155be0d2f7c4b490d
cowsoncows <coffedrop@live.com>
2023-07-04 21:05:20 -0400

    Add favicons

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,20 @@
 import '@/styles/globals.css'
 import Layout from '../components/Layout.js';
+import Head from 'next/head.js';
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Nicholas Brodbeck</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
