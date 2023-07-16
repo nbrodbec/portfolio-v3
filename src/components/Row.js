6157119566a810eacddc32118fd8dd984b4993fe
cowsoncows <coffedrop@live.com>
2023-07-15 22:06:54 -0400

    Fix Safari clipping

--- src/components/Row.js
+++ src/components/Row.js
@@ -2,7 +2,10 @@ import Container from './Container.js';
 
 const Row = ({ children, className, bg, full }) => {
     return (
-        <div className={'z-10 relative flex flex-col items-center ' + (bg || 'bg-white') + ' ' + (className ? className : '')}>
+        <div
+            style={{ '-webkit-transform': 'translate3d(0,0,0)' }} // To prevent clipping behind Hero, especially on Safari
+            className={'z-10 relative flex flex-col items-center ' + (bg || 'bg-white') + ' ' + (className ? className : '')}
+        >
             <Container full={full}>
                 {children}
             </Container>
